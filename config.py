592f09d6f008b4f529931d6ba3963dc1e66c337b
esenbaeverpolat04-cyber <esenbaeverpolat04@gmail.com>
2026-09-15 16:24:04 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-# Telegram bot tokeningiz
-BOT_TOKEN = "8934424323:AAGbMfPqszB14-Q5yc79xGOn5kh5fFuvMWo"
+# Telegram bot tokeningiz (Render Environment'dan o'qiydi)
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8934424323:AAGbMfPqszB14-Q5yc79xG0n5kh5fFuvMWo")
 
 # Sizning Telegram ID raqamingiz (Admin)
-ADMIN_ID = 6911619468
+ADMIN_ID = int(os.getenv("ADMIN_ID", 6911619468))
 
 # Ma'lumotlar bazasi fayli yo'li
 DB_PATH = os.path.join(os.path.dirname(__file__), "database", "bot_database.db")
